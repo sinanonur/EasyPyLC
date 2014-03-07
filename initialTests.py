c00a9e0aaab10c6b80014de29b0ed018dce06093
sinanonur <soaltinuc@gmail.com>
2014-03-07 19:55:52 +0200

    Create initialTests.py

---
+++ initialTests.py
@@ -0,0 +1,10 @@
+dt = PlcDataType('pair');
+dt.setData(
+{'a2': [False, False, True, True, True, True, True, True, True, True],
+ 'lc': {'alt': {'al': 1, 'ti': 2}, 'temp': {'C': 1, 'F': 3}}})
+
+assert(dt.lc.alt.al ==1)
+
+
+dt.setByStringAddress('lc.alt.al',5)
+assert(dt.getByStringAddress('lc.alt.al') ==5)
